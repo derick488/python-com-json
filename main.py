6525e1b2a70bba2b702588cacff1232da22b94d5
derick <derick.torres@escola.pr.gov.br>
2025-08-11 09:35:22 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -39,3 +39,21 @@ def ver_cadastros(cadastros):
             print (f"{i}. nome: {pessoa['nome']}, idade:{pessoa['idade']}, turma: {pessoa['turma']}, curso: {pessoa['cursos']}")
 
     input("\npressione enter voltar ao menu")
+    def main():
+    cadastros = carregar_cadastros()  
+    while True:
+        exibir_menu()
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
+            cadastrar_pessoa(cadastros)
+        elif opcao == "2":
+            ver_cadastros(cadastros)
+        elif opcao == "3":
+            print("Obrigado por utilizar o sistema de cadastro!")
+            break
+        else:
+            print("Opção inválida! Tente novamente.")
+
+
+if __name__ == "__main__":
+    main()
